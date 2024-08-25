b7a8c5f1f682372fece9e2213cd3455f0706e8b8
Joaquim <oliver-junior@hotmail.com>
2024-08-24 23:36:09 -0300

    Update trainner.py

--- trainner.py
+++ trainner.py
@@ -1,12 +1,16 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-x = np.array([80,85,90,95,100,105,110,115,120,125])
-y = np.array([240,250,260,270,280,290,300,310,320,330])
+x = np.array([0,1,2,3])
+y = np.array([3,8,1,10])
 
+plt.subplot(1,2,1)
 plt.plot(x, y)
 
-plt.xlabel('Average Pulse')
-plt.ylabel('Calorie Burnage')
+x = np.array([0,1,2,3])
+y = np.array([10,20,30,40])
+
+plt.subplot(1,2,2)
+plt.plot(x, y)
 
 plt.show()
